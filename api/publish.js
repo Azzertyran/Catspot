cd026959bd7c2c84adf5c99260da42b213e2c0c6
Azzertyran <pilipog49@gmail.com>
2026-04-27 14:39:31 +0200

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if(req.method !== 'POST') return res.status(405).end();
   
   const SUPABASE_URL = "https://bctrrdscvgobmmjqnwks.supabase.co";
-  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdHJyZHNjdmdvYm1tanFud2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMzE5MzUsImV4cCI6MjA5MjYwNzkzNX0.1h1uLYSs-B7XfaX7sOOB8EVsiGrLZowXaq786CeIQGc";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdHJyZHNjdmdvYm1tanFud2tzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzAzMTkzNSwiZXhwIjoyMDkyNjA3OTM1fQ.UywHN9HFc6hdltjJ6NhnXmRqcVNayYDpuSVxOenvSKU";
   
   try {
     const response = await fetch(`${SUPABASE_URL}/rest/v1/cats`, {
